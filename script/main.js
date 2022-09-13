9aeedb3eeedca6982479fcde82d01f632889864d
Alberth Perez <93891315+AlberthPerezM@users.noreply.github.com>
2022-09-12 22:22:29 -0500

    Pokemon1

---
+++ script/main.js
@@ -0,0 +1,41 @@
+const pokemonContainer=document.querySelector('.pokemon-container');
+function fetchPokemon(id){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+ .then((res)=>res.json())
+  .then((data)=> {
+    createPokemon(data);
+});
+   
+}
+
+function fetchPokemons(number){
+    for (let i = 1; i <= number;i++){
+        fetchPokemon(i);
+    }
+}
+function createPokemon(pokemon){
+    const card=document.createElement('div');
+    card.classList.add('pokemon-block');
+
+    const spriteContainer=document.createElement('div');
+    spriteContainer.classList.add('img-container');
+
+    const sprite=document.createElement('img');
+    sprite.src=pokemon.sprites.front_default
+    
+    spriteContainer.appendChild(sprite);
+
+    const number = document.createElement("p");
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+    
+
+    const name = document.createElement("p");
+    name.classList.add("name");
+    name.textContent = pokemon.name;
+  
+    card.appendChild(spriteContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+   pokemonContainer.appendChild(card)
+}
+fetchPokemons(9);
---
+++ script/script.js
@@ -0,0 +1,27 @@
+const pokeCard = document.querySelector('[data-poke-card]');
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+
+
+
+const searchPokemon = event => {
+    event.preventDefault();
+    const { value } = event.target.pokemon;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+        .then(data => data.json())
+        .then(response => renderPokemonData(response))
+        .catch(err => renderNotFound())
+}
+
+const renderPokemonData = data => {
+    const sprite =  data.sprites.front_default;
+    const { stats, types } = data;
+
+    pokeName.textContent = data.name;
+    pokeImg.setAttribute('src', sprite);
+    pokeId.textContent = `Nº ${data.id}`;
+    setCardColor(types);
+    renderPokemonTypes(types);
+    renderPokemonStats(stats);
+}
+
